89ffa3689ee1aa5fa862a846d4c19918d6f82ca3
Toby Alden <toby.alden@gmail.com>
2019-09-26 11:33:35 -0700

    Move dialog cursor outside of box

--- source/entities/DialogBox.hx
+++ source/entities/DialogBox.hx
@@ -88,7 +88,8 @@ class DialogBox extends Entity
         cursor.add("blink", [0, 1], 3);
         cursor.play("blink");
         cursor.x = BOX_WIDTH - 20;
-        cursor.y = BOX_HEIGHT - 14;
+        //cursor.x = BOX_WIDTH - 15;
+        cursor.y = BOX_HEIGHT + 2;
         cursorStartY = cursor.y;
         addGraphic(box);
         addGraphic(frame);
